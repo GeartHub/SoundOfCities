9f0997b70a63f9371b3e04d4687b9b292467de9e
Geart Otten <geartslibbie@gmail.com>
2018-12-14 17:10:54 +0100

    added new audio files and font

---
+++ SoundOfCities/UIFont+Additions.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+Additions.swift
+//  Yoni Project
+//
+//  Generated on Zeplin. (12/14/2018).
+//  Copyright (c) 2018 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+  class var textStyle: UIFont {
+    return UIFont(name: "ModernSansLight", size: 17.5)!
+  }
+
+}
